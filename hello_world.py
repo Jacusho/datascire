346c6826313992edfb1093ca7d49e25476627b92
jluis <atc30@hotmail.com>
2021-10-03 12:52:03 +0200

    test2

--- hello_world.py
+++
@@ -1,4 +0,0 @@
-#print("HELLO WORLD")
-#print("hola")
-#print("hola2")
-print("Mi nombre es Jose luis")
--- suma.py
+++
@@ -1,4 +0,0 @@
-primer_sumando = input("Pon el primer sumando:   ")
-segundo_sumando = input("Pon el segundo sumando:   ")
-resultado = str(int(primer_sumando) + int(segundo_sumando))
-print("El resultado de la suma es: " + resultado)
