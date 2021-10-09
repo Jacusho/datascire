36e593cbd185771b3e75b1b62526ee3b45b49574
jluis <atc30@hotmail.com>
2021-10-09 11:55:54 +0200

    ejercicios2,3

---
+++ APUNTES_UOC/UNIDAD_2/EJERCICIOS/ejercicios_UOC_2.py
@@ -0,0 +1,8 @@
+"""
+Ejercicio UOC 2
+Jluis 09/10/21
+"""
+print("Hello")
+print()
+print()
+print("      Phyton")
---
+++ APUNTES_UOC/UNIDAD_2/EJERCICIOS/ejercicios_UOC_3.py
@@ -0,0 +1,22 @@
+"""
+Ejercicio UOC 3
+Jluis 09/10/21
+1. El último elemento de la primera lista
+2. Una nueva lista con el primer elemento de cada una de las listas
+3. Una nueva lista con los tres primeros elementos de la segunda lista
+4. Una cadena de caracteres con el valor "Miércoles 8"
+"""
+
+l_1 = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
+l_2 = [1, 2, 3, 4, 5, 6, 7, 8]
+
+print(l_1[-1])
+l_3 = []
+l_3.append(l_1[0])
+l_3.append(l_2[-1])
+print(l_3)
+l_4 = []
+l_4.append(l_2[:3])
+print(l_4)
+print(l_1[2] + " " + str(l_2[-1]))
+
